9402fd342909203824a6aba80f0d52f684375122
Saïam Cañedo <redgames3000@gmail.com>
2024-05-19 00:49:05 +0200

    Creation des outille de modification de fichier xml

---
+++ StardewSaveEditor/StardewSaveEditor/StardewValley/XmlTools.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace StardewSaveEditor.StardewValley
+{
+    internal class XmlTools
+    {
+        public static void ReplaceNodeContent(XmlDocument xmlDoc, XmlNode targetNode, XmlNode sourceNode)
+        {
+            targetNode.RemoveAll();
+
+            foreach (XmlAttribute attr in sourceNode.Attributes)
+            {
+                XmlAttribute newAttr = xmlDoc.CreateAttribute(attr.Name);
+                newAttr.Value = attr.Value;
+                targetNode.Attributes.Append(newAttr);
+            }
+            foreach (XmlNode childNode in sourceNode.ChildNodes)
+            {
+                XmlNode newChildNode = xmlDoc.ImportNode(childNode, true);
+                targetNode.AppendChild(newChildNode);
+            }
+        }
+
+        public static void RenameNode(XmlDocument xmlDoc, XmlNode oldNode, string newName)
+        {
+            XmlNode newNode = xmlDoc.CreateElement(newName);
+            ReplaceNodeContent(xmlDoc, newNode, oldNode);
+            oldNode.ParentNode.ReplaceChild(newNode, oldNode);
+        }
+
+        public static void ExchangeNodeContent(XmlDocument xmlDoc, XmlNode nodeA, XmlNode nodeB)
+        {
+            XmlNode tempNode = xmlDoc.CreateElement(nodeA.LocalName);
+            ReplaceNodeContent(xmlDoc, tempNode, nodeA);
+            ReplaceNodeContent(xmlDoc, nodeA, nodeB);
+            ReplaceNodeContent(xmlDoc, nodeB, tempNode);
+        }
+    }
+}
